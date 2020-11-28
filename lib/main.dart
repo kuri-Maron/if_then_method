dc5fb70b58cd73597d71a82f1ed143a45e9a6e71
kurimaron <kurisyun4321@gmail.com>
2020-11-28 09:38:12 +0900

    add: カードレイアウト実装. colse #3

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         // brightness: Brightness.dark, //これでテキストカラーを白にすることも可能だが、他に影響あり
         primaryColor: const Color(0xff00053A),
         scaffoldBackgroundColor: const Color(0xff00053A),
+        // accentTextTheme: TextTheme(),
         // textTheme: TextTheme(
         //   bodyText1: TextStyle(), //このコード未理解
         //   bodyText2: TextStyle(),
@@ -79,86 +80,184 @@ class _MyHomePageState extends State<MyHomePage> {
         itemBuilder: (context, index) {
           return Card(
             // child: Text('${items[index]}'),
-            child: ExpansionTile(
-              title: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text('if'),
-                        Spacer(),
-                        Text('score: ${items[index]}')
-                      ],
+            child: Column(
+              children: <Widget>[
+                ExpansionTile(
+                  title: DefaultTextStyle.merge(
+                    style: TextStyle(
+                      color: const Color(0xff00053A),
                     ),
-                  ),
-                  Padding(
-                    // padding: const EdgeInsets.all(8.0),
-                    padding: const EdgeInsets.only(
-                        left: 20.0, top: 8.0, right: 8.0, bottom: 8.0),
-                    child: Row(
-                      children: [
-                        Text('頭がモヤモヤしたら'),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [Text('then')],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [Text('ゼロメモを書く。')],
-                    ),
-                  ),
-                  //試験的に、拡張タイルを追加してみる、
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        //アイコンの配置バランス調整
-                        Container(
-                          child: SizedBox(
-                            width: 24.0,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text('if'),
+                              // Spacer(),
+                              // Text('score: ${items[index]}')
+                            ],
                           ),
                         ),
-                        Spacer(),
-                        IconButton(
-                          icon: Icon(Icons.arrow_circle_up),
-                          onPressed: () {},
+                        Padding(
+                          // padding: const EdgeInsets.all(8.0),
+                          padding: const EdgeInsets.only(
+                            left: 20.0,
+                            top: 8.0,
+                            right: 8.0,
+                            bottom: 8.0,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                '頭がモヤモヤしたら',
+                                style: TextStyle(),
+                              ),
+                            ],
+                          ),
                         ),
-                        // Container(),
-                        Spacer(),
-                        IconButton(
-                          icon: Icon(Icons.arrow_circle_down),
-                          onPressed: () {},
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [Text('then')],
+                          ),
                         ),
-                        Spacer(),
-                        // Container(),
-                        Icon(Icons.keyboard_arrow_down),
-
-                        //カード内icon行のレイアウト案１
-                        // Expanded(
-                        //   child: SizedBox(
-                        //     height: 50.0,
-                        //     child: GridView.count(
-                        //       crossAxisCount: 6,
-                        //       // shrinkWrap: true,
-                        //       children: gridItemList,
-                        //     ),
-                        //   ),
-                        // ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 20.0,
+                            top: 8.0,
+                            right: 8.0,
+                            bottom: 8.0,
+                          ),
+                          child: Row(
+                            children: [Text('ゼロメモを書く。')],
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                ],
-              ),
-              children: <Widget>[
-                Text('expand area!'),
+                  children: <Widget>[
+                    // Text('expand area!'),
+                    ListTile(
+                      title: DefaultTextStyle.merge(
+                        style: TextStyle(
+                          color: const Color(0xff00053A),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    'exception',
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 20.0,
+                                top: 8.0,
+                                right: 8.0,
+                                bottom: 8.0,
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    '瞑想する。',
+                                  )
+                                ],
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.values[4],
+                              children: [
+                                // Spacer(),
+                                FlatButton(
+                                  child: const Text(
+                                    '編集',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  textColor: const Color(0xff00053A),
+                                  shape: const UnderlineInputBorder(),
+                                  onPressed: () {},
+                                ),
+                                FlatButton(
+                                  child: const Text(
+                                    'アーカイブ',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  textColor: const Color(0xff00053A),
+                                  shape: const UnderlineInputBorder(),
+                                  onPressed: () {},
+                                ),
+                                FlatButton(
+                                  child: const Text(
+                                    '削除',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  textColor: const Color(0xff00053A),
+                                  shape: const UnderlineInputBorder(),
+                                  onPressed: () {},
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    //アイコンの配置バランス調整
+                    Spacer(
+                      flex: 3,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_circle_up),
+                      onPressed: () {},
+                    ),
+                    // Container(),
+                    Spacer(
+                      flex: 3,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_circle_down),
+                      onPressed: () {},
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text('score: ${items[index]}'),
+                    ),
+
+                    //カード内icon行のレイアウト案１
+                    // Expanded(
+                    //   child: SizedBox(
+                    //     height: 50.0,
+                    //     child: GridView.count(
+                    //       crossAxisCount: 6,
+                    //       // shrinkWrap: true,
+                    //       children: gridItemList,
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
+                ),
               ],
             ),
           );
